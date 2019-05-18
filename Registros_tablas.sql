6eaba0961d9fa09394a6803f43fa37b8a58e61e9
Gold <pedro.millaquien@gmail.com>
2019-05-18 14:41:54 -0400

    archivos

---
+++ Registros_tablas.sql
@@ -0,0 +1,32 @@
+
+
+
+
+INSERT INTO USUARIO VALUES ('19383853-7','Pedro','asdf@gmail.com','$2y$10$Mi/sJSyq43Vn6JGdQdaCXek6Gb7vBH.D50001ovxdzZ0jrYj3.DXu');
+INSERT INTO USUARIO VALUES ('19383231-1','Sanz','Sanz@gmail.com','$2y$10$Mi/sJSyq43Vn6JGdQdaCXek6Gb7vBH.D50001ovxdzZ0jrYj3.DXu');
+INSERT INTO USUARIO VALUES ('21383853-7','Dir','Dir@gmail.com','$2y$10$Mi/sJSyq43Vn6JGdQdaCXek6Gb7vBH.D50001ovxdzZ0jrYj3.DXu');
+INSERT INTO USUARIO VALUES ('19321853-3','Generico','Gene@gmail.com','$2y$10$Mi/sJSyq43Vn6JGdQdaCXek6Gb7vBH.D50001ovxdzZ0jrYj3.DXu');
+INSERT INTO USUARIO VALUES ('19341231-9','Trist','Tris@gmail.com','$2y$10$Mi/sJSyq43Vn6JGdQdaCXek6Gb7vBH.D50001ovxdzZ0jrYj3.DXu');
+
+INSERT INTO AlUMNOS VALUES ('1111111-1','98413353','sat@gmail.com');
+INSERT INTO AlUMNOS VALUES ('1231454-1','65465461','sdsa@gmail.com');
+INSERT INTO AlUMNOS VALUES ('5466545-1','87686786','hty@gmail.com');
+INSERT INTO AlUMNOS VALUES ('1232121-1','54654645','pudin@gmail.com');
+INSERT INTO AlUMNOS VALUES ('0876767-1','32423423','salmon@gmail.com');
+
+INSERT INTO INASISTENCIA VALUES ('','Programacion','2019-05-17','1111111-1','19383231-1','401','Correo Electronico');
+INSERT INTO INASISTENCIA VALUES ('','Base de Datos','2019-04-10','1231454-1','19383853-7','401','Correo Electronico');
+INSERT INTO INASISTENCIA VALUES ('','Programacion','2019-01-11','5466545-1','21383853-7','401','Correo Electronico');
+INSERT INTO INASISTENCIA VALUES ('','CCNA','2019-02-16','1111111-1','19383231-1','401','Correo Electronico');
+INSERT INTO INASISTENCIA VALUES ('','Prevencion de Riesgos','2019-03-14','1232121-1','19341231-9','401','Pendiente');
+INSERT INTO INASISTENCIA VALUES ('','Prevencion de Riesgos','2019-03-14','0876767-1','19341231-9','401','Pendiente');
+
+
+INSERT INTO RESPUESTA VALUES ('','Correo Electronico','Familiar Enfermo','1');
+INSERT INTO RESPUESTA VALUES ('','Correo Electronico','Problemas Personales','2');
+INSERT INTO RESPUESTA VALUES ('','Correo Electronico','Lecion Ficisa','3');
+INSERT INTO RESPUESTA VALUES ('','Correo Electronico','Doctor','4');
+INSERT INTO RESPUESTA VALUES ('','Correo Electronico','Familiar Enfermo','1');
+INSERT INTO RESPUESTA VALUES ('','Respondio LLamado','Familiar Enfermo','6');
+
+
